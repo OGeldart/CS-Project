310b49a4e54b7c73c9de8d48c360a8ec98443450
Oliver Geldart <90448000+OGeldart@users.noreply.github.com>
2022-03-24 11:35:44 +0000

    Add files via upload

---
+++ WIPmain.py
@@ -0,0 +1,39 @@
+import time
+import random
+import json
+from abc import ABCMeta, abstractmethod
+
+
+
+#main class
+class character(metaclass=ABCMeta):
+
+    def __init__(self, name, lvl, hp):
+        self._name = name
+        self._lvl = lvl
+        self._hp = hp
+        
+    def stats(self):
+        print("Name:", self._name)
+        print("Level:", self._lvl)
+        print("HP:", self._hp)
+            
+#Defining of functions
+# One of the game's main mechanics
+def venture():
+
+    with open("Zone1.json") as text:
+        entries = text.readlines()
+        search =json.loads(random.choice(entries))
+    monster = character(search[0], search[1], search[2], )
+    monster.stats()
+    print()
+    print(monster._name,"has appeared from the depths!")
+    print()
+
+while True:
+    while True:   
+        venture()
+        reset = input("Press any key to reset...")
+        break
+
